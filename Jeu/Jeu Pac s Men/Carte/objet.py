3aac3af6ddf8a06f45676c485634790e435f3b31
pacs-men <cemeric22@gmail.com>
2017-04-07 11:59:40 +0200

    Update objet.py

--- Jeu/Jeu Pac s Men/Carte/objet.py
+++ Jeu/Jeu Pac s Men/Carte/objet.py
@@ -13,7 +13,7 @@ from carte import*
 
 class objet:
     def __init__(self, carte, x, y):
-        if carte.matrice_objet[x][y] == None:
+        if carte.matrice_objet[x][y] == None and carte.matrice_cases[x][y]:
             self.carte = carte
             carte.placer_obj(self, x, y)
             self.posx = x
@@ -32,5 +32,7 @@ class obj_boug(objet):
         self.direction = direction
         
     def avancer(self):
-        pass
+        if self.carte.dict_cases[matrice_case[self.posx+self.dict_dir[self.direction[0]]][self.posy+self.dict_dir[self.direction[1]]].Marchable == True:
+            if self.carte.matrice_objet[matrice_case[self.posx+self.dict_dir[self.direction[0]][[self.posy+self.dict_dir[self.direction[1]]] == None:
+        
     
