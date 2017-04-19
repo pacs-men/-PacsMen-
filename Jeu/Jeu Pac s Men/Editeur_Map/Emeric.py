b16c6b203182154242724da96d8154ea87ae5a53
pacs-men <cemeric22@gmail.com>
2017-04-19 10:47:52 +0200

    Update Emeric.py

--- Jeu/Jeu Pac s Men/Editeur_Map/Emeric.py
+++ Jeu/Jeu Pac s Men/Editeur_Map/Emeric.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
-
-Ce script temporaire est sauvegardé ici :
-C:\Users\sebastien.thao\.spyder2\.temp.py
-"""
-
-# -*- coding: utf-8 -*-
 
 from Tkinter import *
 import pickle
@@ -16,32 +8,32 @@ l_case=10
 largeur = nombre_case*l_case
 hauteur = nombre_case*l_case
 
-type_case = "Herbe"
-vitesse = 1
+type_case = "herbe"
+vitesse = 0
+bouton_1 = False
 
-def click(event):
-    global m
-    x = event.x/l_case
-    y = event.y/l_case
-    m[x][y]=type_case
+pos_debut = (0, 0)
+pos_fin = (0, 0)
+
+dict_case = {"herbe":"green", "mur":"black"}
+dict_obj = {"coffre":"brown", "arbre" :"green"}
+
+def petit(a, b):
+    if a<b:
+        return a
+    else:
+        return b
+      
+def grand(a, b):
+    if a>b:
+        return a
+    else:
+        return b
 
 
-
-def mur():
+def effobj():
     global type_case
-    type_case = "Mur"
-
-
-    
-def herbe():
-    global type_case
-    type_case = "Herbe"
-    
-
-
-def coffre():
-    global type_case
-    type_case = "Coffre"
+    type_case = "effobj"
     
 
 def lignes():
@@ -55,7 +47,7 @@ def creer_fichier():
         pick = pickle.Pickler(fichier)
         pick.dump(m)
 
-def c_matrice(N):    
+def c_matrice(N):
     m = []
     for i in range(N):
         m.append([])
@@ -67,38 +59,101 @@ def c_matrice(N):
 def afficher_matrice(m):
     for i in range(nombre_case):
         for j in range(nombre_case):
-            if m[i][j] == "Herbe":
+            if m[i][j] == "herbe":
                 case(i,j, "green")
-            elif m[i][j] == "Mur":
+            elif m[i][j] == "mur":
                 case(i,j, "brown")
-            elif m[i][j] == "Coffre":
-                case(i,j, "white")
+            
                 
 
 def case(i, j, color):
-    c.create_rectangle(i*l_case, j*l_case, (i*l_case)+l_case, (j*l_case)+l_case,fill = color)    
+    c.create_rectangle(i*l_case , j*l_case, (i*l_case)+l_case, (j*l_case)+l_case,fill = color)    
 
+def c_matrice2(N):    
+    l = []
+    for x in range(N):
+        l.append([])
+        for y in range(N):
+            l[x].append("")
+    return l
+
+def afficher_matrice2(l):
+    for x in range(nombre_case):
+        for y in range(nombre_case):
+            if l[x][y] == "coffre":
+                objet(x,y, "red")
+            elif l[x][y] == "arbre":
+                objet(x,y, "blue" )
+
+def click(event):
+    global pos_debut
+    global pos_fin
+    pos_debut = (event.x/l_case, event.y/l_case)
+
+
+def relache_1(event):   
+    global bouton_1
+    global m
+    global pos_fin
+    pos_fin = (event.x/l_case, event.y/l_case)
+  
+   
+    for x in range(petit(pos_debut[0], pos_fin[0]), grand(pos_debut[0], pos_fin[0])+1):
+        for y in range(petit(pos_debut[1], pos_fin[1]), grand(pos_debut[1], pos_fin[1])+1):                
+            if type_case in dict_case.keys():
+                m[x][y]=type_case
+            elif type_case in dict_obj.keys():
+                l[x][y] = type_case
+            elif type_case == "effobj":
+                l[x][y] = ""
+
+def objet(x, y, color):
+    c.create_rectangle(x*l_case+3, y*l_case+3, (x*l_case)+l_case-3, (y*l_case)+l_case-3,fill = color) 
+
+def ch_lb(event):
+    global type_case
+    global listbox
+    try:
+        i = listbox.get(listbox.curselection())
+        print i
+        type_case = i
+    except:
+        print "probleme"
+        
 fenetre = Tk()
 
-c = Canvas(fenetre, width= largeur, height= hauteur, bg= "black", borderwidth = 0)
+c = Canvas(fenetre, width= largeur, height= hauteur, bg= "black", borderwidth = -2)
 
 c.bind("<Button-1>", click)
-M = Button(fenetre, text="mur", command=mur)
-M.pack(side = RIGHT)
-H = Button(fenetre, text="herbe", command=herbe)
-H.pack(side = RIGHT)
-Co = Button(fenetre, text="coffre",command=coffre)
-Co.pack()
+c.bind("<ButtonRelease-1>", relache_1)
+
+listbox = Listbox(fenetre)
+for a in dict_case.keys():
+    listbox.insert(END, a)
+    
+for a in dict_obj.keys():
+    listbox.insert(END, a)
+
+listbox.pack(side = LEFT)
+
+listbox.bind('<Button-1>', ch_lb)
+
 RQ = Button(fenetre, text="quitter", command=fenetre.quit)
 RQ.pack()
-
-RQ = Button(fenetre, text="sauvgarde_carte", command=creer_fichier)
-RQ.pack()
+SAVE = Button(fenetre, text="sauvgarde_carte", command=creer_fichier)
+SAVE.pack()
+EFF = Button(fenetre, text="effacer objet", command=effobj)
+EFF.pack()   
     
 m = c_matrice(nombre_case)
 print m
-lignes()
 afficher_matrice(m)
+
+
+l= c_matrice2(nombre_case)
+afficher_matrice2(l)
+
+lignes()
 
 c.pack()
 
@@ -108,6 +163,8 @@ fenetre.title('Editeur - Jeu PACSMEN')
 while True:
     c.after(vitesse)
     afficher_matrice(m)
+    afficher_matrice2(l)
     c.update()
     c.delete(ALL)
-fenetre.mainloop()
+
+
