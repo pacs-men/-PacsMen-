38fdad30bfbe420c2642d3c996c3086244af6146
MenaceNas <missancassa@gmail.com>
2017-05-01 23:25:38 +0200

    Add files via upload

---
+++ Jeu/Jeu Pac s Men/combat/testaffichage.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 01 20:26:04 2017
+
+@author: nassim
+"""
+import pygame
+import combat
+
+pygame.init()
+fenetre=pygame.display.set_mode((640,640))
+pygame.display.set_caption('Programme Pygame de base')
+
+joueur = combat.Mage
+ennemi = Elfs
+
+combat.affiche_combat(fenetre,joueur,ennemi)
+pygame.display.flip()
