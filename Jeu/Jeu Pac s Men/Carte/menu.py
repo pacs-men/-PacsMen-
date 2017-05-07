b88d4549f7bf94b4b4e918e3b2ea5cc64e16ede2
loul213 <canard.meteo@free.fr>
2017-05-07 17:32:06 +0200

    Add files via upload

--- Jeu/Jeu Pac s Men/Carte/menu.py
+++ Jeu/Jeu Pac s Men/Carte/menu.py
@@ -1,4 +1,4 @@
-
+﻿
 import pygame
 from pygame.locals import *
 pygame.init()
@@ -6,6 +6,7 @@ pygame.init()
 #definition des couleurs
 black=(0,0,0)
 white=(0xFF,0xFF,0xFF)
+jaune=(255, 255, 153)
 
 #creation des variables pour ecrire un texte
 font = pygame.font.SysFont('Calibri', 25, True, False)
@@ -26,7 +27,6 @@ def start_menu(fenetre,joueur):
     text5 = ecrire(joueur[4].nom,True,black)
     text6 = ecrire(joueur[5].nom,True,black)
     
-    fenetre.fill(white)
     while True:
         fenetre.blit(img_menu,[0,0])
         pygame.draw.rect(fenetre, black, [170, 227+64*position_bouton, 300, 64], 3)
@@ -69,35 +69,33 @@ def start_menu(fenetre,joueur):
                         return 5
         pygame.display.flip()
 def menupause(fenetre):
-    fenetre.fill(jaune)
     position_bouton = 0
-    pause = 1
     grandtitre=pygame.font.Font('data/fonts/old_london/OldLondon.ttf',55)
-
-    dessiner(fenetre, black, [170, 379, 300, 64], 1) 
-    dessiner(fenetre, black, [170, 463, 300, 64], 1)    
-    dessiner(fenetre, black, [170, 547, 300, 64], 1)
-
-    while pause == 1:
-#        pygame.draw.rect(fenetre, white, [170, 227+64*(position_bouton-1), 300, 64], 3)
-#        pygame.draw.rect(fenetre, white, [170, 227+64*(position_bouton+1), 300, 64], 3)
-#        pygame.draw.rect(fenetre, black, [170, 227+64*position_bouton, 300, 64], 3)
-        titre1=grandtitre.render("MENU PAUSE", True, black)
+    text1 = ecrire("Reprendre",True,black)
+    text2 = ecrire("Menu Principal",True,black)
+    text3 = ecrire("Retour au Bureau",True,black)
+    titre1=grandtitre.render("MENU PAUSE", True, black)
+    while True:
+        fenetre.fill(jaune)
+        dessiner(fenetre, black, [170, 379+84*position_bouton, 300, 64], 3)
+        dessiner(fenetre, black, [170, 379, 300, 64], 1)
+        dessiner(fenetre, black, [170, 463, 300, 64], 1)
+        dessiner(fenetre, black, [170, 547, 300, 64], 1)
         fenetre.blit(titre1, titre1.get_rect(center=(fenetre.get_width()/2, 60)))
-        text1 = ecrire("Reprendre",True,black)
-        text2 = ecrire("Menu Principal",True,black)
-        text3 = ecrire("Retour au Bureau",True,black)
-        
         fenetre.blit(text1, text1.get_rect(center=(fenetre.get_width()/2, 411)))
         fenetre.blit(text2, text2.get_rect(center=(fenetre.get_width()/2, 485)))
-        fenetre.blit(text3, text3.get_rect(center=(fenetre.get_width()/2, 387)))
+        fenetre.blit(text3, text3.get_rect(center=(fenetre.get_width()/2, 587)))
         
         for event in pygame.event.get():
             if event.type == QUIT:
-                pause = 0
+                return "End"
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
-                    pause = 0
+                    return 1
                 if event.key == K_DOWN or event.key == K_RIGHT:
-                    pass
+                    if position_bouton <2:
+                        position_bouton += 1
+                if event.key == K_UP or event.key == K_LEFT:
+                    if position_bouton >0:
+                        position_bouton -= 1
         pygame.display.flip()
---
+++ Jeu/Jeu Pac s Men/Carte/script pa.py
@@ -0,0 +1,55 @@
+import pygame
+from pygame import *
+
+pygame.init()
+fenetre=pygame.display.set_mode((640,640))
+pygame.display.set_caption('Programme intro histoire')
+pygame.font.init()
+noir=(0,0,0)
+blanc=(0xFF, 0xFF, 0xFF)
+font = pygame.font.Font("data/OLondon_.otf",25)
+
+image=pygame.image.load("data/livreintroisn.png")
+
+fenetre.blit(image,[0,110])
+
+pygame.display.flip()
+
+continuer= True
+page= 1
+text5 = font.render("C'est l'Histoire ",True,noir)
+fenetre.blit(text5,[45,145])
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+                continuer = False
+        if event.type==KEYDOWN:
+            if event.key==K_RIGHT:
+                page +=1
+            if event.key==K_LEFT:
+                page -=1
+        if page>10:
+                page=10
+        if page<1:
+                page=1
+        if page== 1:
+                fenetre.blit(image,[0,110])
+                text1g = font.render("C'est l'Histoire ",True,noir)
+                fenetre.blit(text1,[45,145])
+                text1d = font.render("Chocolat ",True,noir)
+                fenetre.blit(text1,[145,145])
+        if page== 2:
+                fenetre.blit(image,[0,110])
+                text2g = font.render("YOLOLOLO",True,noir)
+                fenetre.blit(text2,[45,145])
+        if page== 3:
+                fenetre.blit(image,[0,110])
+                text3 = font.render("YOLOLOLO",True,noir)
+                fenetre.blit(text3,[45,145])
+
+
+
+
+    pygame.display.flip()
+
